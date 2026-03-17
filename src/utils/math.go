4756d9976d8709ee9e594400c8996ba0e4710da5
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 00:04:08 +0530

    feat: integrate src/utils/math.go

---
+++ src/utils/math.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"math"
+)
+
+func CalculateAverage(numbers []float64) float64 {
+	sum := 0.0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum / float64(len(numbers))
+}
